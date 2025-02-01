1a49435e4bf3e0f3ee973f8a88daaf6e760e4436
PrivatsLabs <columnfex@gmail.com>
2025-02-01 22:01:44 +0100

    server off

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static("PROX")); 
-
-app.listen(process.env.PORT || 8080, "0.0.0.0");
-
